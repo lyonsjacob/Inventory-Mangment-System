cd4a8d802c6c9eaae7ac966e390648599dfb6802
Mitchell Willemse <mitchellwillemse@Mitchells-MacBook-Pro.local>
2018-04-27 19:38:19 +1000

    Added Test Cases for Item Object

--- Project/src/item/ItemTest.java
+++ Project/src/item/ItemTest.java
@@ -2,13 +2,37 @@ package item;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+/***
+ * The following tests are for the basic functionality of
+ * the item class.
+ * 
+ * @author mitchellwillemse
+ *
+ */
 public class ItemTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// Test 0: Declaring Item objects.
+	Item items;
+	
+	// Test 1: Constructing an empty Item object.
+	@Before @Test
+	public void setUpEmptyItem() {
+		items = new Item();
 	}
-
+	
+	//Test 2: Constructing an Item object with values;
+	@Test
+	public void setUpItem() {
+		String name = "Coffee";
+		int costPrice = 3;
+		int sellPrice = 4;
+		int reorderPoint = 300;
+		int reorderAmount = 400;
+		
+		items = new Item(name, costPrice, sellPrice, reorderPoint, reorderAmount);
+	}
 }
