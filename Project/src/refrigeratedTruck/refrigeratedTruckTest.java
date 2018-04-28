0b35d7a452e138b63d5e3978f5c5c2e85fdecaf8
Jake <jb.lyons@connect.qut.edu.au>
2018-04-28 10:18:17 +1000

    added refrigerated truck

---
+++ Project/src/refrigeratedTruck/refrigeratedTruckTest.java
@@ -0,0 +1,14 @@
+package refrigeratedTruck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class refrigeratedTruckTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
