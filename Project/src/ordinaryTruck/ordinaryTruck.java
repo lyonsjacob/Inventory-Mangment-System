709059be45c4babad60c14a79825d38e36056815
Jake <jb.lyons@connect.qut.edu.au>
2018-04-28 10:22:41 +1000

    created ordinary truck test case

---
+++ Project/src/ordinaryTruck/ordinaryTruck.java
@@ -0,0 +1,14 @@
+package ordinaryTruck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ordinaryTruck {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
