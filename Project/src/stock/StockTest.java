3d3898bad2455afedaf461fb3be56f9addfa6fd8
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-04-29 12:02:56 +1000

    Added Initial Stock Test Cases.

--- Project/src/stock/StockTest.java
+++ Project/src/stock/StockTest.java
@@ -1,14 +1,42 @@
 package stock;
 
+import item.Item;
+
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
+/***
+ * The following tests are for the basic functionality of
+ * the Stock class.
+ * 
+ *  A collection of items. Can be used for representing store inventory,
+ *  stock orders, sales logs, and truck cargo.
+ * 
+ * @author Mitchell Willemse
+ *
+ */
 public class StockTest {
+	
+	// Test 0: Declaring Stock object.
+	Stock stock;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// Test 1: Constructing an empty Stock object.
+	@Before @Test
+	public void setUpStock() {
+		stock = new Stock();
 	}
+	
+	//Test 2: Add Item.
+	@Test
+	public void addItem() {
+		Item testItem = new Item("Ice Cream", 3, 4, 300, 400, -20);
+		stock.add(testItem);
+		
+		int expectedCount = 1;
+		assertEquals(expectedCount, stock.getCount());
+	}
+	
+	//Test 3: 
 
 }
