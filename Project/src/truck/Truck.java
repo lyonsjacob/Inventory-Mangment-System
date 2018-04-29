c7781cd670c0a6dacb6a73265979e990f64720e6
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-04-29 20:17:27 +1000

    Created Abstract Truck Class.

---
+++ Project/src/truck/Truck.java
@@ -0,0 +1,16 @@
+package truck;
+
+public abstract class Truck {
+	
+	int quantity;
+	double cost;
+	
+	public int getTotalQuantity() {
+		return this.quantity;
+	}
+	
+	public double getCostTest() {
+		return this.cost;
+	}
+
+}
