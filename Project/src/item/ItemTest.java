07071160ae5fde7d7e83e7a0d4d7cb9f28e1bc85
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-04-29 11:11:34 +1000

    Added More Item Test Cases.

--- Project/src/item/ItemTest.java
+++ Project/src/item/ItemTest.java
@@ -16,23 +16,60 @@ import static org.junit.Assert.*;
 public class ItemTest {
 
 	// Test 0: Declaring Item objects.
-	Item items;
+	Item item;
 	
-	// Test 1: Constructing an empty Item object.
+	//Test 1: Constructing an Item object.
 	@Before @Test
-	public void setUpEmptyItem() {
-		items = new Item();
-	}
-	
-	//Test 2: Constructing an Item object with values;
-	@Test
 	public void setUpItem() {
-		String name = "Coffee";
-		int costPrice = 3;
-		int sellPrice = 4;
+		String name = "Ice Cream";
+		double costPrice = 3;
+		double sellPrice = 4;
 		int reorderPoint = 300;
 		int reorderAmount = 400;
+		double temperature = -20;
 		
-		items = new Item(name, costPrice, sellPrice, reorderPoint, reorderAmount);
+		item = new Item(name, costPrice, sellPrice, reorderPoint, reorderAmount, temperature);
+	}
+	
+	//Test 2: Get Name
+	@Test
+	public void getName() {
+		String expectedName = "Ice Cream";
+		assertEquals(expectedName, item.getName());
+	}
+	
+	//Test 3: Get Cost Price
+	@Test
+	public void getCostPrice() {
+		double expectedCostPrice = 3;
+		assertEquals(expectedCostPrice, item.getCostPrice());
+	}
+	
+	//Test 4: Get Sell Price
+	@Test
+	public void getSellPrice() {
+		double expectedSellPrice = 4;
+		assertEquals(expectedSellPrice, item.getSellPrice());
+	}
+	
+	//Test 5: Get Reorder Point
+	@Test
+	public void getReorderPoint() {
+		int expectedReorderPoint = 300;
+		assertEquals(expectedReorderPoint, item.getReorderPoint);
+	}
+	
+	//Test 6: Get Reorder Amount
+	@Test
+	public void getReorderAmount() {
+		int expectedReorderAmount = 400;
+		assertEquals(expectedReorderAmount, item.getReorderAmount);
+	}
+	
+	//Test 7: Get Temperature
+	@Test
+	public void getTemperature() {
+		double expectedTemperature = -20;
+		assertEquals(expectedTemperature, item.getTemperature);
 	}
 }
