2a144045bdaecdc9f7f05385b304a556de51823c
Jake <jb.lyons@connect.qut.edu.au>
2018-05-02 14:34:46 +1000

    Item class updated

--- Project/src/item/Item.java
+++ Project/src/item/Item.java
@@ -13,15 +13,7 @@ public class Item {
 	private int salePrice; // double?
 	private int reorderPoint;
 	private int reorderAmount;
-	
-	// is this needed?
-	public Item() {
-		name = "empty";
-		costPrice = 0; // double?
-		salePrice = 0; // double?
-		reorderAmount = 0;
-		reorderPoint = 0;
-	}
+	private int temperature;
 	
 	/**
 	 * This is a constructor of the item class
@@ -31,13 +23,72 @@ public class Item {
 	 * @param reorderPoint	-quantity when item requires re-stocking 
 	 * @param reorderAmount	- order quantity
 	 */
-	public Item(String name, int costPrice, int salePrice, int reorderPoint, int reorderAmount) {
+	public Item(String name, int costPrice, int salePrice, int reorderPoint, int reorderAmount, int temperature) {
 		this.name = name;
 		this.costPrice = costPrice; // double?
 		this.salePrice = salePrice; // double?
 		this.reorderAmount = reorderAmount;
 		this.reorderPoint = reorderPoint;
+		this.temperature = temperature;
 	}
+	
+	/**
+	 * This method retuns the name of the item
+	 * @return Name of the item.
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * This method gets the cost of the item
+	 * @return Cost of item in dollars.
+	 */
+	public int getCostPrice() {
+		return this.costPrice;
+	}
+	
+	/**
+	 * Gets the sale price of item
+	 * @return Sale price of item in dollars.
+	 */
+	public int getSellPrice() {
+		return this.salePrice;
+	}
+	
+	/**
+	 * Gets reorder point
+	 * @return Reorder point
+	 */
+	public int getReorderPoint() {
+		return this.reorderPoint;
+	}
+	
+	/**
+	 * Gets item reorder amount
+	 * @return Item reorder amount
+	 */
+	public int getReorderAmount() {
+		return this.reorderAmount;
+	}
+	
+	/**
+	 * Gets item temperature 
+	 * @return Item temperature.
+	 */
+	public int getTemperature() {
+		return this.temperature;
+	}
+	
+	
+	public String toString() {
+		
+		String outPutString = this.name+", "+ this.costPrice+", "+ this.salePrice+", "
+				+ ", "+ this.reorderPoint+", "+ this.reorderAmount+", "+this.temperature+"\n";
+		return outPutString;
+	}
+	
+	
 	
 	
 
