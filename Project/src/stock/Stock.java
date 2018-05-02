4ed182181fc7f8fe5827b84788c3cf207c7ec918
Jake <jb.lyons@connect.qut.edu.au>
2018-05-02 15:12:04 +1000

    test 1 in Stock passed

---
+++ Project/src/stock/Stock.java
@@ -0,0 +1,22 @@
+package stock;
+
+import java.util.ArrayList;
+
+import item.Item;
+
+/**
+ * A collection of items. Is be used to hold store inventory information.
+ * 
+ * @author Jacob Lyons N9507175
+ *
+ */
+public class Stock {
+	
+	private ArrayList<Item> storeStock;
+
+	public Stock() {
+		storeStock = new ArrayList<Item>();
+	}
+	
+	public void addItem() {
+}
