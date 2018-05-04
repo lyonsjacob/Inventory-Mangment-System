6983fd9b1d641860e487cf9789c7f39c86ee2eee
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-04 17:08:08 +1000

    Small Fixes to StockTest.

--- Project/src/stock/StockTest.java
+++ Project/src/stock/StockTest.java
@@ -36,7 +36,7 @@ public class StockTest {
 	public void addItem() {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
-		assertEquals(0, stock.getQuantity(testItem));
+		assertEquals(0, stock.getAmount(testItem));
 	}
 	
 	//Test 3: Set Quantity.
@@ -45,9 +45,9 @@ public class StockTest {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
 		int newQuantity = 700;
-		stock.setQuantity(testItem, newQuantity);
+		stock.setAmount(testItem, newQuantity);
 		
-		assertEquals(newQuantity, stock.getQuantity(testItem));
+		assertEquals(newQuantity, stock.getAmount(testItem));
 	}
 	
 	//Test 4: Can't Add Same Item Twice.
@@ -64,21 +64,21 @@ public class StockTest {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
 		
-		stock.setQuantity(testItem, -5);
+		stock.setAmount(testItem, -5);
 	}
 	
 	//Test 6: Can't Set Quantity for Non-Existent Item.
 	@Test (expected = StockException.class) 
 	public void unkmownItem() throws StockException {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
-		stock.setQuantity(testItem);
+		stock.setAmount(testItem);
 	}
 	
 	//Test 7: Get Item By Name.
 	@Test
 	public void getItemByName() {
 		Item testItem1 = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
-		Item testItem2 = new Item(0, "Coffee", 2, 3, 200, 225, null);
+		Item testItem2 = new Item(0, "Coffee", 2, 3, 200, 225);
 		stock.put(testItem1);
 		stock.put(testItem2);
 		
@@ -88,19 +88,21 @@ public class StockTest {
 	//Test 8: Test Iterator 
 	@Test
 	public void testIterator() {
-//		Item testItem1 = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
-//		Item testItem2 = new Item(0, "Coffee", 2, 3, 200, 225, null);
-//		Item testItem3 = new Item(0, "Milk", 3, 4, 100, 150, 3);
-//		
-//		stock.put(testItem1);
-//		stock.put(testItem2);
-//		stock.put(testItem3);
-//		
-//		String expectedOutput = "Ice Cream500Coffee1000Milk1500 ";
-//		String actualOutput = "";
-//		
-//		
-//	    
-//		assertEquals(expectedOutput, actualOutput);
+		Item testItem1 = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
+		Item testItem2 = new Item(0, "Coffee", 2, 3, 200, 225);
+		Item testItem3 = new Item(0, "Milk", 3, 4, 100, 150, 3);
+		
+		stock.put(testItem1);
+		stock.put(testItem2);
+		stock.put(testItem3);
+		
+		String expectedOutput = "Ice CreamCoffeeMilk";
+		String actualOutput = "";
+		
+	    for (Item i : stock) {
+	        actualOutput += i.getName();
+	    }
+		
+		assertEquals(expectedOutput, actualOutput);
 	}
 }
