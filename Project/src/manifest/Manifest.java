7f41e5d9e0f6ba3a1a4fa102c81ef467555a2f25
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-04 17:53:10 +1000

    Added Methods to Manifest Class.

--- Project/src/manifest/Manifest.java
+++ Project/src/manifest/Manifest.java
@@ -1,9 +1,28 @@
 package manifest;
+
+import java.util.ArrayList;
+
+import truck.Truck;
+
 /**
  * 
  * @author Mitchell Willemse
  *
  */
 public class Manifest {
+	
+	ArrayList<Truck> trucks;
+	
+	public Manifest() {
+		
+	}
+	
+	public void addTruck() {
+		
+	}
+	
+	public double getManifestCost() {
+		return 0.0;
+	}
 
 }
