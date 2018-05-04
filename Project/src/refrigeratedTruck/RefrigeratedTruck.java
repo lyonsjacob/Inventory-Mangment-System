216814b05fba74fce9b2070773c1f34f49b2594b
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-04 17:36:04 +1000

    Implemented RefrigeratedTruck Object.

--- Project/src/refrigeratedTruck/RefrigeratedTruck.java
+++ Project/src/refrigeratedTruck/RefrigeratedTruck.java
@@ -1,5 +1,8 @@
 package refrigeratedTruck;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import truck.Truck;
 
 /**
@@ -9,6 +12,48 @@ import truck.Truck;
  */
 public class RefrigeratedTruck extends Truck {
 	
+	private Map<String, Integer> cargo;
+	private int cargoQuantity;
+	private int temperature;
+	private double cost;
 	
+	
+	public RefrigeratedTruck() {
+		cargoQuantity = 0;
+		cargo = new HashMap<String, Integer>();
+	}
+	
+	
+	public void addItem(String itemName, int amount, int temperature) {
+		this.temperature = temperature;
+		this.addItem(itemName, amount);
+	}
+	
+	public int getTemperature() {
+		return temperature;
+	}
+
+	@Override
+	public void addItem(String itemName, int amount) {
+		cargo.put(itemName, amount);
+		cargoQuantity += amount;
+		
+	}
+
+	@Override
+	public int getTotalQuantity() {
+		return cargoQuantity;
+	}
+
+	@Override
+	public Map<String, Integer> GetHashMap() {
+		return cargo;
+	}
+
+	@Override
+	public double getcost() {
+		cost = 900 + 200.00*Math.pow(0.70, temperature/5.00);
+		return cost;	
+	}
 
 }
--- Project/src/truck/Truck.java
+++ Project/src/truck/Truck.java
@@ -1,5 +1,16 @@
 package truck;
+import java.util.Map;
 
+/**
+ * An abstract class for a "truck".
+ * @author Mitchell Willemse
+ *
+ */
 public abstract class Truck {
-
+	
+	public abstract void addItem(String itemName, int amount);
+	public abstract int getTotalQuantity();
+	public abstract Map<String, Integer> GetHashMap();
+	public abstract double getcost();
+	
 }
