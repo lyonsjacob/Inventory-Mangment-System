6011b00b4682112ff14d92e80114849254cd9440
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-07 14:48:25 +1000

    Fixes to StockTest.

--- Project/src/stock/StockTest.java
+++ Project/src/stock/StockTest.java
@@ -22,35 +22,62 @@ import java.util.Map;
  */
 public class StockTest {
 	
-	//Test 0: Declaring Stock object.
+	/**
+	 * Test 0: Declaring Stock object.
+	 */
 	Stock stock;
 
-	//Test 1: Constructing an empty Stock object.
+	
+	
+	/**
+	 * Test 1: Constructing an empty Stock object.
+	 */
 	@Before @Test
 	public void setUpStock() {
 		stock = new Stock();
 	}
 	
-	//Test 2: Add Item.
+	
+	
+	/**
+	 * Test 2: Add Item. Adds Item to the Stock object and uses the get() method
+	 * to ensure the Item was added correctly. 
+	 */
 	@Test
 	public void addItem() {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
-		assertEquals(0, stock.get(testItem));
+		
+		assertEquals(testItem, stock.get("Ice Cream"));
 	}
 	
-	//Test 3: Set Quantity.
+	
+	
+	/**
+	 * Test 3: Set Quantity. Uses the setQuantity() method to change the 'Amount' in the
+	 * Item object.
+	 * 
+	 * I'm keeping setQuantity and getQuantity because although these methods exist
+	 * in the Item class and we could just use the Stock.Get method to change them, it
+	 * seems a lot more neater to encapsulate them within the stock class.
+	 */
 	@Test
 	public void setQuantity() {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
-		int newQuantity = 700;
-		stock.setAmount(testItem, newQuantity);
 		
-		assertEquals(newQuantity, stock.get(testItem));
+		int newQuantity = 700;
+		stock.setQuantity("Ice Cream" newQuantity);
+		
+		assertEquals(newQuantity, stock.getQuantity("Ice Cream"));
 	}
 	
-	//Test 4: Can't Add Same Item Twice.
+	
+	
+	/**
+	 * Test 4: Can't Add Same Item Twice. Tries to add the same Item twice and receives
+	 * StockException.
+	 */
 	@Test (expected = StockException.class)
 	public void duplicateItem() throws StockException {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
@@ -58,34 +85,59 @@ public class StockTest {
 		stock.put(testItem);
 	}
 	
-	//Test 5: Can't Have Negative Quantity.
+	
+	
+	/*
+	 * Test 5: Can't Have Negative Quantity. Tries to use setQuantity() to set a negative
+	 * 'Amount' Receives StockException.
+	 */
 	@Test (expected = StockException.class)
 	public void negativeQuantity() throws StockException {
 		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
 		stock.put(testItem);
 		
-		stock.setAmount(testItem, -5);
+		stock.setQuantity("Ice Cream", -5);
 	}
 	
-	//Test 6: Can't Set Quantity for Non-Existent Item.
+	
+	
+	/*
+	 * Test 6: Can't Set Quantity for Non-Existent Item. Tries to use setQuantity() for an
+	 * Item that has not been added to Stock. StockException() is thrown.
+	 */
 	@Test (expected = StockException.class) 
 	public void unkmownItem() throws StockException {
-		Item testItem = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
-		stock.setAmount(testItem);
+		stock.setQuantity("Cookies", 5);
 	}
 	
-	//Test 7: Get Item By Name.
+	
+	
+	/*
+	 * Test 7: Get Stock Quantity. use getStockQuantity() to get the number of Items stored
+	 * in the Stock object.
+	 */
 	@Test
-	public void getItemByName() {
-		Item testItem1 = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
-		Item testItem2 = new Item(0, "Coffee", 2, 3, 200, 225);
+	public void getStockQuantity() {
+		Item testItem1 = new Item(3, "Ice Cream", 3, 4, 300, 400, -20);
+		Item testItem2 = new Item(4, "Tofu", 3, 4, 300, 400, 3);
+		Item testItem3 = new Item(7, "Chips", 3, 4, 300, 400);
+		
 		stock.put(testItem1);
 		stock.put(testItem2);
+		stock.put(testItem3);
 		
-		assertEquals(testItem1, stock.get("Ice Cream"));
+		int expectedOutput = 3;
+		int actualOutput = stock.getStockQuantity();
+		
+		assertEquals(expectedOutput, actualOutput);
 	}
 	
-	//Test 8: Test Iterator 
+	
+	
+	/*
+	 * Test 8: Test Iterator. Tests to make sure the Stock class can be used to iterate
+	 * through each Item in the Stock object. Ensures each Item is properly iterated through.
+	 */
 	@Test
 	public void testIterator() {
 		Item testItem1 = new Item(0, "Ice Cream", 3, 4, 300, 400, -20);
