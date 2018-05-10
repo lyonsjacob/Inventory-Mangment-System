ba5f2a21af0daf0ea8ebc003536800a831e57538
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-10 16:48:07 +1000

    Created ManifestCreator Class.

---
+++ Project/src/manifestCreator/ManifestCreator.java
@@ -0,0 +1,10 @@
+package manifestCreator;
+
+/**
+ * Creates trucks based on items needed to be ordered and puts them
+ * in a manifest.
+ * @author Mitchell Willemse 
+ */
+public class ManifestCreator {
+
+}
