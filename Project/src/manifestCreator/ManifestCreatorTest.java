5bcd8cba335f23fb33451ff11022fc88795f4d51
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-12 14:21:20 +1000

    Created ManifestCreatorTest.

---
+++ Project/src/manifestCreator/ManifestCreatorTest.java
@@ -0,0 +1,14 @@
+package manifestCreator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ManifestCreatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
