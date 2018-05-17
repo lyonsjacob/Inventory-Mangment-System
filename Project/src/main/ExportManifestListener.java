f143e5fb13e9735a7627b78dcaa9b174c10fc9f3
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-17 17:45:19 +1000

    Implemented WriteOutCSV and ExportManifestListener.

--- Project/src/main/ExportManifestListener.java
+++ Project/src/main/ExportManifestListener.java
@@ -1,8 +1,14 @@
 package main;
+import writeOutCSV.WriteOutCSV;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.JFileChooser;
+
+import exceptions.CSVFormatException;
 import exceptions.DeliveryException;
 import exceptions.StockException;
 import manifest.Manifest;
@@ -22,7 +28,9 @@ public class ExportManifestListener implements ActionListener {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+
+		
 		System.out.println("Export Manifest");
 		
 		Manifest manifest = new Manifest();
@@ -33,6 +41,24 @@ public class ExportManifestListener implements ActionListener {
 		} catch (StockException | DeliveryException a) {
 			// TODO Auto-generated catch block
 			a.printStackTrace();
+		}
+		
+		JFileChooser fileChooser = new JFileChooser();
+		if (fileChooser.showSaveDialog(fileChooser) == JFileChooser.APPROVE_OPTION) {
+			try {
+				File file = fileChooser.getSelectedFile();
+				
+				
+				
+				WriteOutCSV.writeToCSV(file, manifest.getTruckString());
+			} catch (CSVFormatException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		  
 		}
 	}
 
--- Project/src/writeOutCSV/WriteOutCSV.java
+++ Project/src/writeOutCSV/WriteOutCSV.java
@@ -1,4 +1,8 @@
 package writeOutCSV;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import exceptions.CSVFormatException;
 
 /**
@@ -11,12 +15,19 @@ public class WriteOutCSV {
 	/**
 	 * Opens a dialog box for the user to choose a location on the file system. Takes in
 	 * a string and exports this to a CSV file in the selected location.
+	 * @param fileNameAndPath The file name and location on the file system.
 	 * @param message The string to export to CSV.
 	 * @return Whether the operation succeeded.
 	 * @throws CSVFormatException
+	 * @throws IOException 
 	 * @author Mitchell Willemse (n9470620);
 	 */
-	public static Boolean writeToCSV(String message) throws CSVFormatException {
+	public static Boolean writeToCSV(File fileNameAndPath, String message) throws CSVFormatException, IOException {
+		FileWriter writer = new FileWriter(fileNameAndPath);
+		
+		writer.write(message);
+		writer.close();
+		
 		return false;
 	}
 
