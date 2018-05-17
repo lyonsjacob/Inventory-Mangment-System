b7fef6818df039a51685c7c2ef29ebd4c21cb751
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-17 17:25:54 +1000

    Created WriteOutCSV Class.

---
+++ Project/src/writeOutCSV/WriteOutCSV.java
@@ -0,0 +1,23 @@
+package writeOutCSV;
+import exceptions.CSVFormatException;
+
+/**
+ * A class that holds the method to write a string to CSV file.
+ * @author Mitchell Willemse (n9470620).
+ *
+ */
+public class WriteOutCSV {
+	
+	/**
+	 * Opens a dialog box for the user to choose a location on the file system. Takes in
+	 * a string and exports this to a CSV file in the selected location.
+	 * @param message The string to export to CSV.
+	 * @return Whether the operation succeeded.
+	 * @throws CSVFormatException
+	 * @author Mitchell Willemse (n9470620);
+	 */
+	public static Boolean writeToCSV(String message) throws CSVFormatException {
+		return false;
+	}
+
+}
