c08014a9bfb96c19c5b57954bd04f1441714f6c0
Mitchell Willemse <mitchell.p.willemse@gmail.com>
2018-05-17 17:19:42 +1000

    Added ManifestCreator to ExportManifestListener.

--- Project/src/main/ExportManifestListener.java
+++ Project/src/main/ExportManifestListener.java
@@ -3,6 +3,12 @@ package main;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import exceptions.DeliveryException;
+import exceptions.StockException;
+import manifest.Manifest;
+import manifestCreator.ManifestCreator;
+import store.Store;
+
 /**
  * A listener for the Export Manifest button.
  * @author Mitchell Willemse & Jacob Lyons.
@@ -18,6 +24,16 @@ public class ExportManifestListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("Export Manifest");
+		
+		Manifest manifest = new Manifest();
+
+		try {
+			manifest = ManifestCreator.CreateManifest(Store.getInstance().getInventory());
+			System.out.println(manifest.getTruckString());
+		} catch (StockException | DeliveryException a) {
+			// TODO Auto-generated catch block
+			a.printStackTrace();
+		}
 	}
 
 }
