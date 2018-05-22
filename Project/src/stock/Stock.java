9fe243eee35240e7fc72d3ecdb4ed4bba32cb4dd
Jake <jb.lyons@connect.qut.edu.au>
2018-05-22 21:16:24 +1000

    added author to stock classes

--- Project/src/stock/Stock.java
+++ Project/src/stock/Stock.java
@@ -11,28 +11,32 @@ import item.Item;
 /**
  * A collection of items. Is be used to hold store inventory information.
  * 
- * @author Jacob Lyons N9507175 & Mitchell Willemse (n9470620),
+ * @author Jacob Lyons N9507175,
  *
  */
-public class Stock implements Iterable<Item>  {
+public class Stock implements Iterable<Item> {
 	
 	private ArrayList<Item> storeStock;
 
 	/**
-	 * Initilises an empty array list
+	 * Initilises an empty array list.
+	 * @author Jacob Lyons
 	 */
 	public Stock() {
 		storeStock = new ArrayList<Item>();
 	}
 	
+	
 	public int getStockQuantity() {
 		return storeStock.size();
 	}
+	
 	
 	/**
 	 * Puts an item in the storeStock collection
 	 * @param item
 	 * @exception throws exception if the same item is added twice.
+	 * @author Jacob Lyons
 	 */
 	public void put(Item item)throws StockException {
 		String itemName = item.getName();
@@ -48,20 +52,23 @@ public class Stock implements Iterable<Item>  {
 		storeStock.add(item);	
 	}
 	
+	
 	/**
 	 * This method clears all items in the stores inventory.
+	 * @author Jacob Lyons
 	 */
 	public void clear() {
 		storeStock.clear();
 	}
+	
 
 	/**
 	 * Gets item object using name as key.
 	 * @param string itemName - item name
 	 * @return Item 
 	 * @throws StockException when item is not in list.
+	 * @author Jacob Lyons
 	 */
-	// note must change method name because it over rides list.get();
 	public Item getItemName(String itemName)throws StockException {
 		for(int i = 0; i < storeStock.size(); i++) {
 			
@@ -80,10 +87,12 @@ public class Stock implements Iterable<Item>  {
 	 * @param itemName
 	 * @param quantity
 	 * @throws StockException
+	 * @author Jacob Lyons
 	 */
 	public void setQuantity(String itemName, int quantity) throws StockException {
 		getItemName(itemName).setAmount(quantity);
 	}
+	
 	
 	/**
 	 * This class gets the quantity of the item.
@@ -91,6 +100,7 @@ public class Stock implements Iterable<Item>  {
 	 * @param itemName
 	 * @param quantity
 	 * @throws StockException
+	 * @author Jacob Lyons
 	 */
 	public int getQuantity(String itemName) throws StockException{
 		return getItemName(itemName).getAmount();
@@ -99,6 +109,7 @@ public class Stock implements Iterable<Item>  {
 
 	/**
 	 * This is an iterator for the stock collection.
+	 * @author Jacob Lyons
 	 */
 	@Override
 	public Iterator<Item> iterator() {
