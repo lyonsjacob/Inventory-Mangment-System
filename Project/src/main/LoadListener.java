086f91fecd48710096d51e6ce5f7aba8c1e72a29
Jake <jb.lyons@connect.qut.edu.au>
2018-05-24 22:47:20 +1000

    Added comments to the load CSV classes

--- Project/src/main/LoadListener.java
+++ Project/src/main/LoadListener.java
@@ -18,28 +18,40 @@ import readInCSV.ReadItemCSV;
 public class LoadListener implements ActionListener {
 
 	/**
-	 * Invoked when an action occurs.
-	 * @author Mitchell Willemse (n9470620) & Jacob Lyons (n9507175).
+	 * Invoked when "load Item" button press occurs this method 
+	 * asks the user to select a file using Java's in built File Selector GUI.
+	 * The selected file format is check in the CSVFormatCheck()
+	 * before being passed to the ReadItemCSV() class witch
+	 * reads the file and updates the stores inventory and capital.
+	 * UpdateGUIElements is then called to update the JTable and
+	 * JLable displayed in the GUI.
+	 * 
+	 * All exceptions are handled in the method by passing the 
+	 * error message to the user in the form of a pop up window.
+	 * 
+	 * @author Jacob Lyons (N9507175).
 	 */
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-
+		// Initialize classes needed
 		ErrorMessageBox popUpBox = new ErrorMessageBox();
 		CSVFormatCheck CSVcheck = new CSVFormatCheck();
 		FileSelector fileSelector  = new FileSelector();
 		
+		// Get file from file selector GUI.
 		String fileNamePath = fileSelector.getFile();
 		
 			try {
+				// Check file format
 				if(CSVcheck.checkCSVFormat(fileNamePath)) {
 				ReadItemCSV readitemCSV = new ReadItemCSV(fileNamePath);
 				}
 			} catch (CSVFormatException | IOException | StockException e1) {
 				e1.printStackTrace();
+				// Display exception message in pop up window
 				popUpBox.ErrorMessage(e1.getMessage());
 			}
 			
+	// Update the GUI.
 	UpdateGUIElements table = UpdateGUIElements.getInstance();
 	table.updateTable();
 	table.updateLable();
--- Project/src/main/LoadManifestListener.java
+++ Project/src/main/LoadManifestListener.java
@@ -18,28 +18,43 @@ import readInCSV.ReadManifestCSV;
 public class LoadManifestListener implements ActionListener {
 
 	/**
-	 * Invoked when an action occurs.
-	 * @author Mitchell Willemse (n9470620) & Jacob Lyons(N9507175).
+	 * Invoked when "load manifest" button press occurs this method 
+	 * asks the user to select a file using Java's in built File Selector gui.
+	 * The selected file format is check in the CSVFormatCheck()
+	 * before being passed to the ReadManidestSCV() class witch
+	 * reads the file and updates the stores inventory and capital.
+	 * UpdateGUIElements is then called to update the JTable and
+	 * JLable displayed in the GUI.
+	 * 
+	 * All exceptions are handled in the method by passing the 
+	 * error message to the user in the form of a pop up window.
+	 * 
+	 * @author Jacob Lyons (N9507175).
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		// Initialize classes needed.
 		ErrorMessageBox popUpBox = new ErrorMessageBox();
 		CSVFormatCheck CSVcheck = new CSVFormatCheck();
 		FileSelector fileSelector  = new FileSelector();
 		
+		// Get file name and path from file selector GUI.
 		String fileNamePath = fileSelector.getFile();
 		
-
 			try {
+				// Check file format.
 				if(CSVcheck.checkCSVFormat(fileNamePath)) {
+					// Update store inventory and capital.
 					ReadManifestCSV readManifestCSV = new ReadManifestCSV(fileNamePath);
 				}
 			} catch (CSVFormatException | IOException | StockException e1) {
+				// Display exception messages in pop up window.
 				popUpBox.ErrorMessage(e1.getMessage());
 				e1.printStackTrace();
 			}
 			
+		// Update the elements in the GUI.
 		UpdateGUIElements table = UpdateGUIElements.getInstance();
 		table.updateTable();
 		table.updateLable();
